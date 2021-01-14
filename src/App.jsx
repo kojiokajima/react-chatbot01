64c9b347a53daa22bcc56bd9dedac6bacb0ef186
wanko <kouji124@w4.dion.ne.jp>
2021-01-13 16:31:55 -0800

    added first chat

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { RecentActorsRounded } from '@material-ui/icons';
 import { render } from '@testing-library/react';
 import defaultDataset from './dataset';
 import './assets/styles/style.css';
-import {AnswersList} from './components/index'
+import {AnswersList, Chats} from './components/index'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -26,8 +26,25 @@ export default class App extends React.Component {
       answers: initAnswers
     })
   }
+  
+  initChats = () => {
+    const initDataset = this.state.dataset[this.state.currentId]
+    const chat = {
+      text: initDataset.question,
+      type: "question"
+    }
+
+    const chats = this.state.chats;
+    chats.push(chat)
+
+    this.setState({
+      chats: chats
+    })
+  }
+
 
   componentDidMount() {
+    this.initChats();
     this.initAnswer()
   }
 
@@ -35,6 +52,7 @@ export default class App extends React.Component {
     return (
       <section className="c-section">
         <div className="c-box">
+          <Chats chats={this.state.chats}/>
           <AnswersList answers={this.state.answers}/>
         </div>
       </section >
